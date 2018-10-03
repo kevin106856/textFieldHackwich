71347c60a751e4e79256641e7cc719b2c6ed0f0f
ARTEAGA,KA KEVIN <rl106856@rlas-macair-20.rlas-116.org>
2018-10-03 10:25:29 -0500

    Initial Commit

---
+++ textFieldHackwich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textFieldHackwich
+//
+//  Created by ARTEAGA,KA KEVIN on 10/3/18.
+//  Copyright © 2018 ARTEAGA,KA KEVIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
